5a57b4279d8b94bea9808fbc4372d8f630ad1319
valerieroman <81587687+valerieroman@users.noreply.github.com>
2021-08-05 15:12:54 -0400

    Created new photos component, receiving them from the pexels API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos";
 import './Dictionary.css';
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword); 
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleDictionResponse(response) {
-        console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
     function handlePexelsResponse(response) {
-        console.log(response);
+        setPhotos(response.data[0]);
     }
 
     function search() {
@@ -23,16 +24,12 @@ export default function Dictionary(props) {
 
         let pexelsApiKey = "563492ad6f91700001000001a89c33479aff4508b3ff9e8e9b9540f1";
 
-        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
-        axios.get(pexelsApiUrl, 
-            { headers: {"Authorization" :    `Bearer ${pexelsApiKey}` },
-        })
-        .then(handlePexelsResponse);
-
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
-
-    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -48,27 +45,29 @@ export default function Dictionary(props) {
         setLoaded(true);
         search();
         
-        if (loaded) {
-            return (
-            <div className="Dictionary">
-             <section>
-                 <h1>What word do you want to look up?</h1>
-               <form onSubmit={handleSubmit}>
-                <input type="search" autoFocus={true}
-                placeholder="Search for a word🔍"
-                onChange={handleKeywordChange}
-                 />
-               </form>
-                <div className="hint">
-                    suggested words: sunset, journal, yoga, plant, serendipity...
-                </div>
-               </section>
-               <Results results={results} />
-            </div>
-            );
-        } else {
-            load();
-            return "Loading";
-        }
     }
-
+    
+    if (loaded) {
+        return (
+        <div className="Dictionary">
+         <section>
+             <h1>What word do you want to look up?</h1>
+           <form onSubmit={handleSubmit}>
+            <input type="search" autoFocus={true}
+            placeholder="Search for a word🔍"
+            onChange={handleKeywordChange}
+             />
+           </form>
+            <div className="hint">
+                suggested words: sunset, journal, yoga, plant, serendipity...
+            </div>
+           </section>
+           <Results results={results} />
+           <Photos photos={photos} />
+        </div>
+        );
+    } else {
+        load();
+        return "Loading";
+    }
+}
